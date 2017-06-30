1a9ce9c5cbe692ff967ea50e5f6f570ef563703e
David Kahara <kaharadave@gmail.com>
2017-06-30 10:54:21 +0300

    Downloaded gulp packages and required them in a gulpfile

--- js/lookup.js
+++ js/lookup.js
@@ -1,1 +1,21 @@
 var apiKey = require(./../.env).apiKey;
+
+function RepoName(userInput) {
+  this.userInput = userInput;
+}
+
+RepoName.prototype.getUsername = function(username) {
+  $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response) {
+    $('#avatar').html('<img src="' + response.avatar_url + '">');
+    if(response.name === null){
+      response.name = "No name set";
+    }
+    $('#output').text(response.name);
+    //event.preventDefault();
+  }).fail(function(error) {
+    $('#output').text(error.responseJSON.message);
+    $('#inputForm').empty();
+    $('#avatar').empty();
+  });
+};
+exports.repoNameModule = RepoName;
